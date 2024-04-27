e6fd8746f1af8dda2a0bcadf3029bea1ff8b634b
Hoa Duong <hoadx.fu@gmail.com>
2024-04-27 07:07:58 +0700

    feat: init project

---
+++ script/Deploy.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity >=0.8.18;
+
+import "forge-std/Script.sol";
+
+// Deploy a contract to a deterministic address with create2 factory.
+contract Deploy is Script {
+    // Create X address.
+    Deployer public deployer =
+        Deployer(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
+
+    // Vault factory address for v1.0.0
+    address public factory = 0xDA5701B852a65128e96c1085366089643eEC7EAD;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Append constructor args to the bytecode
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("TokenizedStrategy.sol:TokenizedStrategy"),
+            abi.encode(factory)
+        );
+
+        // Pick an unique salt
+        bytes32 salt = keccak256("v1.0.0");
+
+        address contractAddress = deployer.deployCreate2(salt, bytecode);
+
+        console.log("Address is ", contractAddress);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract Deployer {
+    event ContractCreation(address indexed newContract, bytes32 indexed salt);
+
+    function deployCreate2(
+        bytes32 salt,
+        bytes memory initCode
+    ) public payable returns (address newContract) {}
+}
